f02b4ea01428384e7ea1ce56a30cf5724c73479f
Nazmush Shakib <shakibgithub944>
2022-11-24 10:51:23 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
-// const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
 // const jwt = require('jsonwebtoken');
-// require('dotenv').config()
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000
 const app = express();
 
 app.use(cors())
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ofvswtt.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const CategoryCollection = client.db("laptop-bazar").collection("category");
+
+        
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error));
 
 
 app.get('/', (req, res) => {
