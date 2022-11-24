e43c3d3325df3d70e7eaa3bbd7ee35f66a5c0832
Nazmush Shakib <shakibgithub944>
2022-11-24 13:06:45 +0600

    category api created

--- index.js
+++ index.js
@@ -15,15 +15,34 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ofvswtt.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const CategoryCollection = client.db("laptop-bazar").collection("category");
+        const ProductCollection = client.db("laptop-bazar").collection("products");
 
-        
+        app.get('/category', async (req, res) => {
+            const query = {}
+            const result = await CategoryCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.post('/addproduct', async (req, res) => {
+            const product = req.body;
+            const result = await ProductCollection.insertOne(product);
+            res.send(result);
+        })
+        app.get('/category-product/:name', async (req, res) => {
+            const brand = req.params.name;
+            console.log(brand);
+            const query = {
+                category: brand
+            }
+            const result = await ProductCollection.find(query).toArray();
+            res.send(result);
+
+        })
 
     }
     finally {
